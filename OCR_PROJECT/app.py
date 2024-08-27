63033bd45a907b2e21e90ff51bd5095f3c943b68
JOSE GABRIEL SILVA PEREIRA <01917955260@trt8.jus.br>
2024-08-27 11:09:40 -0300

    upload de pdf, sendo convertido imagens dentro desse pdf

--- OCR_PROJECT/app.py
+++ OCR_PROJECT/app.py
@@ -3,35 +3,59 @@ import pytesseract
 from PIL import Image
 import os
 from io import BytesIO
+from pdf2image import convert_from_bytes
+from PyPDF2 import PdfMerger 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 
 # Caminho em que está o executável do Tesseract (deve ser mudado de acordo com o pc usado, para realizar testes)
-caminho_tesseract = r"C:/Users/00208560246/AppData/Local/Programs/Tesseract-OCR"
+caminho_tesseract = r"C:/Users/01917955260/AppData/Local/Programs/Tesseract-OCR"
 
 pytesseract.pytesseract.tesseract_cmd = caminho_tesseract + r"/tesseract.exe"
+
+# Caminho do Poppler no Windows
+caminho_poppler = r"c:/Users/01917955260/Documents/poppler-24.07.0/Library/bin"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Verificar se o arquivo foi enviado
-        if 'image' not in request.files:
-            return "Nenhuma imagem foi enviada"
+        if 'arquivo' not in request.files:
+            return "Nenhum arquivo foi enviada"
 
-        file = request.files['image']
+        file = request.files['arquivo']
 
         if file.filename == '':
-            return "Nenhuma imagem selecionada"
+            return "Nenhuma arquivo selecionada"
+        
+        if not file.filename.endswith('.pdf'):
+            return "Arquivo enviado não é pdf. Por favor, envie um arquivo pdf"
+        
+        # Carrega as páginas do pdf para imagem
+        imagens = convert_from_bytes(file.read(), poppler_path = caminho_poppler)
 
-        # Carrega a imagem diretamente da memória usando o BytesIO
-        imagem = Image.open(BytesIO(file.read()))
 
-        # Converte para pdf
-        pdf = pytesseract.image_to_pdf_or_hocr(imagem, extension='pdf')
+        pdfs = []
+        for imagem in imagens:
+            # Converte cada imagem em PDF usando Tesseract
+            pdf = pytesseract.image_to_pdf_or_hocr(imagem, extension='pdf')
+            pdfs.append(BytesIO(pdf))
+
+        # Mescla os PDFs usando PdfMerger do PyPDF2
+        output_pdf = BytesIO()
+        merger = PdfMerger()
+
+        for pdf in pdfs:
+            pdf.seek(0)
+            merger.append(pdf)
+
+        merger.write(output_pdf)
+        merger.close()
+        output_pdf.seek(0)
 
         # Retornar o PDF como resposta
-        return send_file(BytesIO(pdf),  download_name='Gerado.pdf', as_attachment=True)
+        return send_file(output_pdf,  download_name='Gerado.pdf', as_attachment=True)
 
     return render_template('index.html')
 
--- OCR_PROJECT/env/Scripts/activate.bat
+++ OCR_PROJECT/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\01917955260\Documents\OCR PROJECT\env
+set VIRTUAL_ENV=C:\Users\01917955260\Documents\projeto_ferramentas\OCR_PROJECT\env
 
 if not defined PROMPT set PROMPT=$P$G
 
